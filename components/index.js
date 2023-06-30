439848acf993001dd60f6c6c12b6676006c027c4
gsoftinteractive <gsoftinteractive@gmail.com>
2023-06-30 19:38:28 +0100

    first phase completed

---
+++ components/index.js
@@ -0,0 +1,16 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+
+export {
+  HomeHeader,
+  DetailsBid,
+  FocusedStatusBar,
+  DetailsDesc,
+  NFTCard,
+  CircleButton,
+  RectButton,
+};
